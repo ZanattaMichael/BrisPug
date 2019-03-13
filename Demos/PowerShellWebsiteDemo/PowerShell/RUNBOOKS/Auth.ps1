869555f9e49c9dab8271e643eb4541eaea275966
Michael Zanatta <zanattaam@gmail.com>
2019-03-13 16:31:33 +1000

    Initial Commit
    Added scripts. Pending Testing.

---
+++ Demos/PowerShellWebsiteDemo/PowerShell/RUNBOOKS/Auth.ps1
@@ -0,0 +1,69 @@
+Param (
+	[parameter(Mandatory=$True,Position=1)]
+	[String]
+	$UserName,
+	[parameter(Mandatory=$True,Position=2)]
+	[String]
+    $Password,
+    [parameter(Mandatory=$True,Position=3)]
+	[String]
+    $MfaPin
+)
+
+$helperPath = $(Get-AutomationVariable -Name 'HelperPath')
+$userProfilePath = $(Get-AutomationVariable -Name 'UserProfile')
+
+# Dot Source my Helper in!
+Get-ChildItem -LiteralPath $helperPath -File | ForEach-Object {. $_.FullName }
+
+#
+# Load User Profile in
+#
+
+# Test the Path Make Sure it Exists
+if (Test-Path -LiteralPath $userProfilePath -ErrorAction SilentlyContinue) {
+    # Return back to the caller
+    return (ConvertTo-Json -InputObject @{ error = "Cannot find the path"})
+}
+
+#
+# Load the User Profile
+
+try {
+    $UserObject = Get-Content -LiteralPath $userProfilePath | ConvertFrom-Json
+} catch {
+    # Return to the caller
+    return (ConvertTo-Json -InputObject @{ error = $_.ErrorMessage })
+}
+
+#
+# Test the User Objects
+
+if (Test-ObjectProperty -object $UserObject -properties username, passwords, mfasecretkey, cooldata) {
+    # Return to the caller
+    return (ConvertTo-Json -InputObject @{ error = "User Validation Failed"})
+}
+
+#
+# Validate User Credentials
+
+# Validate if the User Exists
+if ($UserName -ne $UserObject.username) {
+    # Return to the caller
+    return (ConvertTo-Json -InputObject @{ error = "Incorrect Username or Password. Please try again."})
+}
+
+# Validate MFA
+if ((Get-GoogleAuthenticatorPin -Secret $UserObject.mfasecretkey) -eq $MfaPin) {
+    return (ConvertTo-Json -InputObject @{ error = "Incorrect MFA. Please try again."})
+}
+
+# Validate the Password
+if ($UserObject.password -cne $Password) {
+    # Return to the caller
+    return (ConvertTo-Json -InputObject @{ error = "Incorrect Username or Password. Please try again."})
+}
+
+# Since everything else has evualted to be false, this must be true
+return (ConvertTo-Json -InputObject @{ success = $UserObject})
+
