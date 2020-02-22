265a0e99e47315bdb72f24ce00c94b9dab64102d
PowerShellMichael <zanattaam@gmail.com>
2020-02-22 10:45:56 +1000

    Add Snippet
    Add

---
+++ Snippets/Add-FunctionsToPSJob.ps1
@@ -0,0 +1,24 @@
+
+<#
+This is a quick snippet I whipped up this morning to dynamically add functions your your PowerShell Jobs
+without having to recreate the script block. There are two flavors, implicit or explicit. 
+ Implicit will get all functions (that are not attached to a module) 
+ Explicit: You define what functions you wish to pull.
+#>
+
+Function AlternativeFunction($test) {
+    Write-Output "Function Called"
+}
+
+# Enumerate all the Functions that are not loaded from a module. This will include alias functions as well. 
+$Functions = (Get-Item "Function:*").Where{$_.Source -eq "" -and $_.CommandType -eq "Function"} | ForEach-Object { [String]"Function $($_.Name) { $($_.ScriptBlock) };" }
+
+# Select Specific Functions
+$FunctionsToInclude = 'TestFunction','TestAnotherFunction'
+$Functions = (Get-Item "Function:*").Where{$_.Name -in $FunctionsToInclude } | ForEach-Object { [String]"Function $($_.Name) { $($_.ScriptBlock) };" }
+
+# Build out a scriptblock of the functions
+$ScriptBlock = [scriptblock]::Create($Functions)
+
+# Include into your PowerShell Job
+$Job = Start-Job -InitializationScript $ScriptBlock -ScriptBlock { $output = TestAnotherFunction -ParameterName "TEST" ; Write-Output $output }
