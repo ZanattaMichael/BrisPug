6db585243cf464683b62c7ac63fdd3c4a29efb79
PowerShellMichael <zanattaam@gmail.com>
2020-02-26 14:33:28 +1000

    Add Test-MVPStatus Snippet
    Add Test-MVP Status for Don Jones

---
+++ Snippets/Test-MVPStatus.ps1
@@ -0,0 +1,27 @@
+Function Test-MVPStatus {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [String]
+        $Name
+    )
+
+    process {
+
+        $params = @{
+            Uri = 'https://www.mvp.microsoft.com/en-us/MvpSearch?kw={0}' -f [System.Web.HttpUtility]::UrlEncode($Name)
+        }
+
+        (Invoke-WebRequest @params).Content -notmatch '(No results found for the selected query.)'
+    }
+}
+
+<#
+PSv7.0-rc.3 -> C:\Users\Michael.Zanatta>Test-MVPStatus -Name "Michael Zanatta"
+False
+PSv7.0-rc.3 -> C:\Users\Michael.Zanatta>Test-MVPStatus -Name "Don Jones"
+True
+PSv7.0-rc.3 -> C:\Users\Michael.Zanatta>"Michael Zanatta","Don Jones" | Test-MVPStatus
+False
+True
+#>
